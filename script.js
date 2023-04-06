7677afb485655a64ff1403a42005ed97b383715c
brunoprosa <bruno.prosa01@gmail.com>
2023-04-06 19:31:19 -0300

    virar cartas concluido

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ let numCards = 0;
 let cards = document.querySelector(".cards");
 let gifs = ["/images/bobrossparrot.gif", "/images/explodyparrot.gif", "/images/fiestaparrot.gif", "/images/metalparrot.gif", "/images/revertitparrot.gif", "/images/tripletsparrot.gif", "/images/unicornparrot.gif"];
 let gifsAleatorio = [];
+let jogadas = 0;
 window.onload = numberCards();
 function numberCards(){
     numCards = Number(prompt("quantidade de cartas para jogar"));
@@ -18,10 +19,40 @@ function distribuirCartas(){
     }
     gifsAleatorio.sort(randomizar);
     for(let i = 0; i < numCards; i++){
-        cards.innerHTML += `<div class="card"><div class ="front-face face" onclick ="virarCarta(this)"><img src="/images/back.png"></div><div class ="back-face face"><img src=${gifsAleatorio[i]}></div></div>`;
+        cards.innerHTML += `<div class="card semPar" onclick ="virarCarta(this)"><div class ="front-face face"><img src="/images/back.png"></div><div class ="back-face face"><img src=${gifsAleatorio[i]}></div></div>`;
     }
 }
 function randomizar(){
     return Math.random() - 0.5;
 }
-//function virarCarta(card){}
+function virarCarta(carta) {
+    let cartaSelecionadaAnt = document.querySelector(".selecionado");
+
+    const frente = carta.querySelector(".front-face");
+    frente.classList.toggle("front");
+
+    const tras = carta.querySelector(".back-face");
+    tras.classList.toggle("back");
+
+    carta.classList.add("selecionado");
+    jogadas++;
+    
+    if (cartaSelecionadaAnt !== null){
+        if (cartaSelecionadaAnt.innerHTML !== carta.innerHTML){
+            setTimeout(desvirarCartas, 1000);
+        }else{
+            carta.classList.remove("selecionado");
+            cartaSelecionadaAnt.classList.remove("selecionado");
+        }
+    }
+    
+}
+function desvirarCartas(){
+    document.querySelector(".selecionado .front-face").classList.toggle("front");
+    document.querySelector(".selecionado .back-face").classList.toggle("back");
+    document.querySelector(".selecionado").classList.remove("selecionado");
+
+    document.querySelector(".selecionado .front-face").classList.toggle("front");
+    document.querySelector(".selecionado .back-face").classList.toggle("back");
+    document.querySelector(".selecionado").classList.remove("selecionado");
+}
