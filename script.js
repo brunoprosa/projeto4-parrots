9249ebd0733a71d2aee9d8bf4a89fbb99f26c776
brunoprosa <bruno.prosa01@gmail.com>
2023-04-06 19:52:30 -0300

    fim de jogo completo

--- script.js
+++ script.js
@@ -36,15 +36,21 @@ function virarCarta(carta) {
 
     carta.classList.add("selecionado");
     jogadas++;
-    
+
     if (cartaSelecionadaAnt !== null){
         if (cartaSelecionadaAnt.innerHTML !== carta.innerHTML){
             setTimeout(desvirarCartas, 1000);
         }else{
             carta.classList.remove("selecionado");
             cartaSelecionadaAnt.classList.remove("selecionado");
+            carta.classList.remove("semPar");
+            cartaSelecionadaAnt.classList.remove("semPar");
+            if (document.querySelector(".semPar") === null){
+                setTimeout(fimDoJogo,50);
+            }
         }
     }
+    console.log(document.querySelector(".semPar"));
     
 }
 function desvirarCartas(){
@@ -55,4 +61,7 @@ function desvirarCartas(){
     document.querySelector(".selecionado .front-face").classList.toggle("front");
     document.querySelector(".selecionado .back-face").classList.toggle("back");
     document.querySelector(".selecionado").classList.remove("selecionado");
+}
+function fimDoJogo(){
+    alert(`Você ganhou em ${jogadas} jogadas!`);
 }
